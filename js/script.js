c0845edb9eda19db09e0eb09962e2d2a0d7ad031
Evgeniya Chudakova <misa1239@gmail.com>
2019-12-04 17:49:35 +0300

    испр. шрифты. Добавлено наведение, сдайлер

--- js/script.js
+++ js/script.js
@@ -21,3 +21,64 @@ function dropdown_menu(){
 }
 
 dropdown_menu();
+
+let menu_btn = document.querySelector(".mob-menu_btn");
+let menu_btn_close = document.querySelector(".close_btn");
+let mob_menu = document.querySelector(".mob-menu");
+function mobileMenu(){
+    
+    menu_btn.addEventListener("click",function(){
+        menu_btn.style.display = "none";
+        menu_btn_close.style.display = "block";
+        mob_menu.style.display = "flex"; 
+        
+    });
+
+    menu_btn_close.addEventListener("click", function(){
+        menu_btn.style.display = "block";
+        menu_btn_close.style.display = "none";
+        mob_menu.style.display = "none"; 
+
+    });
+}
+
+mobileMenu();
+
+
+$('.review_cards').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        //   infinite: true
+        //   dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    //   ,
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+
+    ]
+  });
+
+  $('.slick-next').hide();
+  $('.slick-prev').hide();
